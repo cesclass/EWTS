1a01aaa1312650b879cc9872e89c3df019f18eaf
cesclass <cyril.esclassan@gmail.com>
2016-02-17 11:35:32 +0100

    obtimisation et boucle de temps dans le js

--- public_html/script.js
+++ public_html/script.js
@@ -4,21 +4,25 @@ var emplacementCgi = "cgi-bin/trame.cgi";
 var methodSend = "GET";
 var contenueSend = null;
 var xhr = new XMLHttpRequest();
+var returnCgi = "null";
 
 function findTrame()
 {
-    xhr.open(methodSend, emplacementCgi, true);
-    xhr.send(contenueSend);
-    xhr.addEventListener("readystatechange", function()
+    document.getElementById("trame").innerHTML = "...";
+    setInterval(function()
     {
-        //  Si le script (cgi) à fini son travail (readyState = DONE) 
-        //  et qu'il n'y a pas d'erreur HTTP (status = 200)
-        if(xhr.readyState === xhr.DONE && xhr.status === 200)
+        xhr.open(methodSend, emplacementCgi, true);
+        xhr.send(contenueSend);
+        xhr.addEventListener("readystatechange", function()
         {
-            //  Le contenue (innerHTML) de la balise ayant l'id "trame" prend
-            //  le contenur text retourné par le script cgi
-            document.getElementById("trame").innerHTML += xhr.responseText;
-        }
-    }, false);
+            if(xhr.readyState === xhr.DONE && xhr.status === 200)
+            {
+                returnCgi = xhr.responseText;
+                if(document.getElementById("trame").innerHTML !== returnCgi)
+                {
+                    document.getElementById("trame").innerHTML = returnCgi;
+                }
+            }
+        }, false);
+    }, 3000);
 }
-
