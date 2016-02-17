3c391d7c466bfe56f33368ad29e0d6c27c795890
cesclass <cyril.esclassan@gmail.com>
2016-02-17 17:09:39 +0100

    nettoyage du code

--- public_html/script.js
+++ public_html/script.js
@@ -5,22 +5,27 @@ var methodSend = "GET";
 var contenueSend = null;
 var xhr = new XMLHttpRequest();
 
-function findTrame()
+
+function main()
 {
     document.getElementById("trame").innerHTML = "...";
-    setInterval(function()
+    xhr.open(methodSend, emplacementCgi, true);
+    xhr.addEventListener("readystatechange",event , false);
+    setInterval(sendXHR(), 5000);
+}
+
+function sendXHR()
+{
+    xhr.send(contenueSend);
+}
+
+function event()
+{
+    if(xhr.readyState === xhr.DONE && xhr.status === 200)
     {
-        xhr.open(methodSend, emplacementCgi, true);
-        xhr.send(contenueSend);
-        xhr.addEventListener("readystatechange", function()
+        if(document.getElementById("trame").innerHTML !== xhr.responseText)
         {
-            if(xhr.readyState === xhr.DONE && xhr.status === 200)
-            {
-                if(document.getElementById("trame").innerHTML !== xhr.responseText)
-                {
-                    document.getElementById("trame").innerHTML = xhr.responseText;
-                }
-            }
-        }, false);
-    }, 5000);
+            document.getElementById("trame").innerHTML = xhr.responseText;
+        }
+    }
 }
