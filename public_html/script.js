548cb8681b8888da358b09aaf7ec15b78b4aa850
cesclass <cyril.esclassan@gmail.com>
2016-02-17 17:28:48 +0100

    retour en arrière, nouveau codo non fonctionnel

--- public_html/script.js
+++ public_html/script.js
@@ -5,21 +5,41 @@ var methodSend = "GET";
 var contenueSend = null;
 var xhr = new XMLHttpRequest();
 
+function findTrame()
+{
+    document.getElementById("trame").innerHTML = "...";
+    setInterval(function()
+    {
+        xhr.open(methodSend, emplacementCgi, true);
+        xhr.send(contenueSend);
+        xhr.addEventListener("readystatechange", function()
+        {
+            if(xhr.readyState === xhr.DONE && xhr.status === 200)
+            {
+                if(document.getElementById("trame").innerHTML !== xhr.responseText)
+                {
+                    document.getElementById("trame").innerHTML = xhr.responseText;
+                }
+            }
+        }, false);
+    }, 5000);
+}
 
+/*
 function main()
 {
     document.getElementById("trame").innerHTML = "...";
-    xhr.open(methodSend, emplacementCgi, true);
-    xhr.addEventListener("readystatechange",event , false);
     setInterval(sendXHR(), 5000);
+    xhr.addEventListener("readystatechange",response() , false);
 }
 
 function sendXHR()
 {
+    xhr.open(methodSend, emplacementCgi, true);
     xhr.send(contenueSend);
 }
 
-function event()
+function response()
 {
     if(xhr.readyState === xhr.DONE && xhr.status === 200)
     {
@@ -29,3 +49,4 @@ function event()
         }
     }
 }
+ */
