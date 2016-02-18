fa13b79ef7c17b72628ef7c8df35f667bdf390b3
cesclass <cyril.esclassan@gmail.com>
2016-02-18 14:36:27 +0100

    Correction, nettoyage et validation du code JS

---
+++ public_html/QaD.js
@@ -0,0 +1,26 @@
+//  Quick and Dirty
+
+var emplacementCgi = "cgi-bin/trame.cgi";
+var methodSend = "GET";
+var contenueSend = null;
+var xhr = new XMLHttpRequest();
+
+function findTrame()
+{
+    document.getElementById("trame").innerHTML = "...";
+    setInterval(function()
+    {
+        xhr.open(methodSend, emplacementCgi, true);
+        xhr.send(contenueSend);
+        xhr.addEventListener("readystatechange", function()
+        {
+            if(xhr.readyState === xhr.DONE && xhr.status === 200)
+            {
+                if(document.getElementById("trame").innerHTML !== xhr.responseText)
+                {
+                    document.getElementById("trame").innerHTML = xhr.responseText;
+                }
+            }
+        }, false);
+    }, 500);
+}
--- public_html/script.js
+++ public_html/script.js
@@ -5,32 +5,11 @@ var methodSend = "GET";
 var contenueSend = null;
 var xhr = new XMLHttpRequest();
 
-function findTrame()
-{
-    document.getElementById("trame").innerHTML = "...";
-    setInterval(function()
-    {
-        xhr.open(methodSend, emplacementCgi, true);
-        xhr.send(contenueSend);
-        xhr.addEventListener("readystatechange", function()
-        {
-            if(xhr.readyState === xhr.DONE && xhr.status === 200)
-            {
-                if(document.getElementById("trame").innerHTML !== xhr.responseText)
-                {
-                    document.getElementById("trame").innerHTML = xhr.responseText;
-                }
-            }
-        }, false);
-    }, 5000);
-}
-
-/*
 function main()
 {
     document.getElementById("trame").innerHTML = "...";
-    setInterval(sendXHR(), 5000);
-    xhr.addEventListener("readystatechange",response() , false);
+    setInterval(sendXHR, 5000);
+    xhr.addEventListener("readystatechange",response , false);
 }
 
 function sendXHR()
@@ -49,4 +28,3 @@ function response()
         }
     }
 }
- */
