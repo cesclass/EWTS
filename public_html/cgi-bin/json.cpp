be03c2499f3e36ba61ceb62f773e5e4ac555d62c
cesclass <cyril.esclassan@gmail.com>
2016-03-09 16:21:28 +0100

    Update JSON

--- public_html/cgi-bin/json.cpp
+++ public_html/cgi-bin/json.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main()
 {
     cout << "Content-Type: application/json \n\r\n\r"; 
-    cout << "{\"Trames\"[" <<
-            "{\"id\":\"01\", \"data\":\"294\"}" <<
-            "{\"id\":\"02\", \"data\":\"753\"}" <<
+    cout << "{\"Trames\":[" <<
+            "{\"id\":\"01\", \"data\":\"294\"}," <<
+            "{\"id\":\"02\", \"data\":\"753\"}," <<
             "{\"id\":\"03\", \"data\":\"618\"}" <<
             "]}";
     return 0;
--- public_html/script/script.js
+++ public_html/script/script.js
@@ -1,14 +1,17 @@
 //  script.js
 
-var emplacementCgi = "cgi-bin/trame.cgi";
+var emplacementCgi = "cgi-bin/json.cgi";
 var methodSend = "GET";
 var contenueSend = null;
 var xhr = new XMLHttpRequest();
+var jsonOBJ;
+var jsonTXT;
+var datas;
 
 function main()
 {
     document.getElementById("trame").innerHTML = "Wait...";
-    setInterval(sendXHR, 500);  //  Boucle
+    setInterval(sendXHR, 1000);
     xhr.addEventListener("readystatechange",response , false);
 }
 
@@ -20,20 +23,26 @@ function sendXHR()
 
 function response()
 {
-    //  Si le script a fini son exécution (DONE) 
-    //  et qu'il n'y a pas eu d'erreurs (200 OK)
     if(xhr.readyState === xhr.DONE && xhr.status === 200)
     {
-        //  Si le contenue de la balise (id = trame) n'est pas 
-        //  le même que celui retourné par le script
-        if(document.getElementById("trame").innerHTML !== xhr.responseText)
+        jsonTXT = xhr.responseText;
+        if(JSON.parse(jsonTXT) !== jsonOBJ)
         {
-            document.getElementById("trame").innerHTML = xhr.responseText;
+            jsonOBJ = JSON.parse(xhr.responseText);
+            datas = "";
+            datas += "<tr> <td>ID</td> <span class=\"tabul\" /> <td>DATAS</td> </tr> <br />";
+            for(var i = 0; i < jsonOBJ.Trames.length; i++){
+                datas += "<tr> <td>" + jsonOBJ.Trames[i].id + "</td> <span class=\"tabul\" /> <td>" 
+                    + jsonOBJ.Trames[i].data + "</td> </tr> <br />";
+            }
+            datas += "<br />";
+            /*
+            "<tr> <td>" + jsonOBJ.Trames[0].id + "</td> <span class=\"tabul\" /> <td>" + jsonOBJ.Trames[0].data + "</td> </tr> <br />" +
+            "<tr> <td>" + jsonOBJ.Trames[1].id + "</td> <span class=\"tabul\" /> <td>" + jsonOBJ.Trames[1].data + "</td> </tr> <br />" +
+            "<tr> <td>" + jsonOBJ.Trames[2].id + "</td> <span class=\"tabul\" /> <td>" + jsonOBJ.Trames[2].data + "</td> </tr> <br />" +
+            "<br />";
+            */
+            document.getElementById("trame").innerHTML = datas;
         }
     }
-}
-
-function test()
-{
-    alert("test appel fonction");
 }
